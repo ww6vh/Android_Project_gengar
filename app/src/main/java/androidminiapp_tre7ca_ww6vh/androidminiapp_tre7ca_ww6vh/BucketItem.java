d1dc16847d75c312da6596b11f8c93c3c101165e
WendyWu <ww6vh@virginia.edu>
2017-01-30 19:03:58 -0500

    Trying to keep everything working, it seemed that everything got git ignored

---
+++ app/src/main/java/androidminiapp_tre7ca_ww6vh/androidminiapp_tre7ca_ww6vh/BucketItem.java
@@ -0,0 +1,67 @@
+package androidminiapp_tre7ca_ww6vh.androidminiapp_tre7ca_ww6vh;
+
+import java.util.ArrayList;
+
+/**
+ * Code adapted from Professor Sheriff's ListExample code
+ */
+
+public class BucketItem {
+    private String mName;
+    private String mDescription;
+    private double mLatitude;
+    private double mLongitude;
+    private boolean mCompleted;
+
+    public BucketItem(String name, String description, double latitude, double longitude,
+                      boolean completed) {
+        mName = name;
+        mDescription = description;
+        mLatitude = latitude;
+        mLongitude = longitude;
+        mCompleted = completed;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public boolean isCompleted() {
+        return mCompleted;
+    }
+
+    private static int lastBucketItemId = 0;
+
+/*    public static ArrayList<BucketItem> createBucketItemList(int numBucketItems) {
+        ArrayList<BucketItem> bucketItems = new ArrayList<BucketItem>();
+        String description = "";
+        double latitude = 0.0;
+        double longitude = 0.0;
+        Boolean completed = Boolean.FALSE;
+
+        for (int i = 1; i <= numBucketItems; i++) {
+            bucketItems.add(new BucketItem("BucketItem " + ++lastBucketItemId, description, latitude, longitude, completed));
+        }
+
+        return bucketItems;
+    }*/
+
+    public static ArrayList<BucketItem> createInitialBucketList(){
+        ArrayList<BucketItem> bucketItems = new ArrayList<BucketItem>();
+
+        String name1 = "steam tunneling";
+        String desc1 = "adventure in the steam tunnels";
+        double lat1 = 38.0336;
+        double lon1 = 78.5080;
+        Boolean comp1 = Boolean.FALSE;
+        String name2 = "lighting of the lawn";
+        String desc2 = "see the lightshow and acappella at lighting of the lawn";
+        double lat2 = 38.0336;
+        double lon2 = 78.5080;
+
+        bucketItems.add(new BucketItem(name1, desc1, lat1, lon1, comp1));
+        bucketItems.add(new BucketItem(name2, desc2, lat2, lon2, comp1));
+
+        return bucketItems;
+    }
+}
---
+++ app/src/main/java/androidminiapp_tre7ca_ww6vh/androidminiapp_tre7ca_ww6vh/BucketItemAdapter.java
@@ -0,0 +1,95 @@
+package androidminiapp_tre7ca_ww6vh.androidminiapp_tre7ca_ww6vh;
+
+/**
+ * Code adapted from Professor Sheriff's ListExample code
+ */
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+// Create the basic adapter extending from RecyclerView.Adapter
+// Note that we specify the custom ViewHolder which gives us access to our views
+public class BucketItemAdapter extends
+        RecyclerView.Adapter<BucketItemAdapter.ViewHolder> {
+
+    // Provide a direct reference to each of the views within a data item
+    // Used to cache the views within the item layout for fast access
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // Your holder should contain a member variable
+        // for any view that will be set as you render a row
+        public TextView nameTextView;
+
+        // We also create a constructor that accepts the entire item row
+        // and does the view lookups to find each subview
+        public ViewHolder(View itemView) {
+            // Stores the itemView in a public final member variable that can be used
+            // to access the context from any ViewHolder instance.
+            super(itemView);
+
+            nameTextView = (TextView) itemView.findViewById(R.id.bucketItem_name);
+
+        }
+    }
+
+    // Store a member variable for the contacts
+    private List<BucketItem> mBucketItems;
+    // Store the context for easy access
+    private Context mContext;
+
+    // Pass in the contact array into the constructor
+    public BucketItemAdapter(Context context, List<BucketItem> bucketItems) {
+        mBucketItems = bucketItems;
+        mContext = context;
+    }
+
+    // Easy access to the context object in the recyclerview
+    private Context getContext() {
+        return mContext;
+    }
+
+    // Usually involves inflating a layout from XML and returning the holder
+    @Override
+    public BucketItemAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        // Inflate the custom layout
+        View bucketItemView = inflater.inflate(R.layout.content_bucket_list, parent, false);
+
+        // Return a new holder instance
+        ViewHolder viewHolder = new ViewHolder(bucketItemView);
+        return viewHolder;
+    }
+
+    // Involves populating data into the item through holder
+    @Override
+    public void onBindViewHolder(BucketItemAdapter.ViewHolder viewHolder, int position) {
+        // Get the data model based on position
+        BucketItem bucketItem = mBucketItems.get(position);
+
+        // Set item views based on your views and data model
+        TextView textView = viewHolder.nameTextView;
+        textView.setText(bucketItem.getName());
+        if(!bucketItem.isCompleted()) {
+            textView.setClickable(false);
+            textView.setActivated(false);
+            textView.setEnabled(false);
+
+        }
+
+    }
+
+    // Returns the total count of items in the list
+    @Override
+    public int getItemCount() {
+        Log.d("test", mBucketItems.size()+"");
+        return mBucketItems.size();
+    }
+}
